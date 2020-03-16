9e61703fdfdfa52ffe37e137e50152f068b9de11
Victoria <vnkcampbell@gmail.com>
2020-03-16 12:24:13 -0700

    Modify frontend folder with new react app creation

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,10 +1,9 @@
-import { createStore, applyMiddleware } from 'redux';
-import rootReducer from '../reducers/root_reducer';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "../../../frontend/src/reducers/root_reducer";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
 
-const configureStore = (preloadedState = {}) => (
-    createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger))
-);
+const configureStore = (preloadedState = {}) =>
+  createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger));
 
 export default configureStore;
