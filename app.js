cc032097aff96a545f6fb28327d6266a79964fbd
chrisjterry <chrisjterry102@gmail.com>
2020-03-16 15:54:45 -0700

    Complete trails backend

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
+const trails = require("./routes/api/trails");
+const bodyParser = require('body-parser');
+
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use("/api/trails", trails);
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
---
+++ validation/trails.js
@@ -0,0 +1,47 @@
+const Validator = require('validator');
+const validText = require('./valid-text');
+
+module.exports = function validateTrailInput(data) {
+    let errors = {};
+    console.log(data)
+    data.title = validText(data.title) ? data.title : '';
+    data.description = validText(data.description) ? data.description : '';
+    data.difficulty = validText(data.difficulty) ? data.difficulty : '';
+    data.petFriendly = validText(data.petFriendly) ? data.petFriendly : '';
+    data.paved = validText(data.paved) ? data.paved : '';
+    data.lat = validText(data.lat) ? data.lat : '';
+    data.lng = validText(data.lng) ? data.lng : '';
+    
+    if (Validator.isEmpty(data.title)) {
+      errors.title = 'Title must be included';
+    }
+
+    if (Validator.isEmpty(data.description)) {
+        errors.description = 'Description must be included';
+    }
+
+    if (!Validator.isIn(data.difficulty, ['easy', 'moderate', 'hard'])) {
+        errors.difficulty = 'Difficulty must be selected';
+    }
+    
+    if (!Validator.isBoolean(data.petFriendly)) {
+        errors.petFriendly = 'Pet-friendly status must be indicated';
+    }
+
+    if (!Validator.isBoolean(data.paved)) {
+        errors.paved = 'Trail paving status must be indicated';
+    }
+
+    if (!Validator.isNumeric(data.lat)) {
+        errors.lat = 'Latitude must be a valid number';
+    }
+
+    if (!Validator.isNumeric(data.lng)) {
+        errors.lng = 'Longitude must be a valid number';
+    }
+  
+    return {
+      errors,
+      isValid: Object.keys(errors).length === 0
+    };
+};
