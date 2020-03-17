2ff12b3e58af8d486a6b2c70954a872b4b01618f
Victoria <vnkcampbell@gmail.com>
2020-03-16 21:00:35 -0700

    Add trails index structure

--- frontend/src/components/trails/trails_index.jsx
+++ frontend/src/components/trails/trails_index.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Tucker1 from "./tucker1.png";
+
 
 
 class TrailIndex extends React.Component {
@@ -20,10 +22,10 @@ class TrailIndex extends React.Component {
                 {/* hi Jeff we can render whatever you want about each trail here (see Trail.js for full list of Trail properties/ie table columns)! (full details can go in each trails show page)*/}
                 {/* each trails number (starting at 1) */}
                 <div>Trail #{i + 1}</div>
-                {/* each trail's name */}
-                <div>{trail.name}</div>
-                {/* each trail's description */}
-                <div>{trail.description}</div>
+                <img src={Tucker1} height="100px" width="100px"></img>
+                <div>Name - {trail.name}</div>
+                <div>Description - {trail.description}</div>
+                <div>Rating - ★★★★★</div>
               </div>
             ))}
           </div>
