0aca6a5a26b12b2071d1e4c965f52cc1decacdb2
jordantom <jordan.tom88@gmail.com>
2020-03-19 13:58:51 -0700

    Complete render of weather data to Trail Show page

--- frontend/src/components/trails/trail_show.jsx
+++ frontend/src/components/trails/trail_show.jsx
@@ -3,13 +3,15 @@ import { Link } from "react-router-dom";
 import Buttercup1 from "./buttercup1.png";
 import ReviewsIndexContainer from '../reviews/reviews_index_container';
 
-
 class TrailShow extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+  constructor(props) {
+    super(props);
+    this.state = Object.assign({loaded:false}, this.state);
+  }
   componentWillMount() {
-    this.props.fetchTrail(this.props.match.params.id);
+    //   debugger
+    this.props.fetchTrail(this.props.match.params.id).
+    then(() => {this.setState({loaded: true})})
     window.scrollTo(0, 0);
   }
 
@@ -19,14 +21,14 @@ class TrailShow extends React.Component {
       lng: this.props.trail.lng
     })
   }
-
+  
   render() {
     const { trail, weather } = this.props;
-
-    if (!trail) return null;
-    if (!weather) return null;
+    // if (!trail) return null;
     // if (!trail.photos) return null;
-
+    if(!weather) return null;
+    // debugger
+    
     if (!this.state.loaded) {
       return (
         <div>not loaded</div>
@@ -38,6 +40,15 @@ class TrailShow extends React.Component {
         <Link to={`/trails/${trail.id}`}>
           <img src={Buttercup1} height="300px" width="300px"></img>
         </Link>
+        <div className="weather-container">
+          <div className="weather-info">
+            {weather.summary}
+            {weather.temperature}
+            {weather.windSpeed}
+            {weather.precipProbability}
+            {weather.humidity}
+          </div>
+        </div>
         <div>Description - {trail.description}</div>
         <div>Rating - ★★★★★</div>
         <ReviewsIndexContainer />
@@ -45,5 +56,4 @@ class TrailShow extends React.Component {
     );
   }
 }
-
 export default TrailShow;
