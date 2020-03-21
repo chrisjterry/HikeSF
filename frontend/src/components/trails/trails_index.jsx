e65377a1b2b744a941ff20870f22c1d576e1d994
chrisjterry <chrisjterry102@gmail.com>
2020-03-20 18:35:30 -0700

    Add AWS photo url to index page

--- frontend/src/components/trails/trails_index.jsx
+++ frontend/src/components/trails/trails_index.jsx
@@ -15,42 +15,42 @@ class TrailIndex extends React.Component {
     this.props.fetchTrails();
     window.scrollTo(0, 0);
   }
-
-    render() {
-        // debugger
-        return (
-          <div className="trails-index">
-            <div className="trails-index-1">
-              <p>Navigate around the map to find your next adventure. 
-                ➳</p>
-              <div className="index-map">
-                <IndexMap
-                  trails={this.props.trails}
-                  fetchTrails={this.props.fetchTrails}
-                />
-              </div>
-            </div>
-            <div className="trails-index-2">
-              <div className="trails-list">
-                {this.props.trails.map((trail, i) => (
-                  <div className="trails-list-internal">
-                    <div>
-                      <Link to={`/trails/${trail._id}`}>
-                        <img src={Tucker1} height="100px" width="100px"></img>
-                      </Link>
-                    </div>
-                    <div className="trail-text">
-                      <div className="trail-title">{trail.title}</div>
-                      <div>{trail.description}</div>
-                      {/* <div className='trails-index-rating'>Rating - ★★★★★</div> */}
-                    </div>
-                  </div>
-                ))}
-              </div>
+  
+  render() {
+      // debugger
+      return (
+        <div className="trails-index">
+          <div className="trails-index-1">
+            <p>Navigate around the map to find your next adventure. 
+              ➳</p>
+            <div className="index-map">
+              <IndexMap
+                trails={this.props.trails}
+                fetchTrails={this.props.fetchTrails}
+              />
             </div>
           </div>
-        );
-    }
+          <div className="trails-index-2">
+            <div className="trails-list">
+              {this.props.trails.map((trail, i) => (
+                <div className="trails-list-internal">
+                  <div>
+                    <Link to={`/trails/${trail._id}`}>
+                      <img src={trail.picture_url} height="100px" width="100px"></img>
+                    </Link>
+                  </div>
+                  <div className="trail-text">
+                    <div className="trail-title">{trail.title}</div>
+                    <div>{trail.description}</div>
+                    {/* <div className='trails-index-rating'>Rating - ★★★★★</div> */}
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      );
+  }
 }
 
 export default TrailIndex;
