bfb1eb390df6671abc54d92b42f9b50dd5616d60
chrisjterry <chrisjterry102@gmail.com>
2020-03-21 16:27:04 -0700

    Reconfigure keys for heroku

---
+++ config/keys_prod.js
@@ -0,0 +1,9 @@
+module.exports = {
+    mongoURI: process.env.MONGO_URI,
+    secretOrKey: process.env.SECRET_OR_KEY,
+    mapsAPIKey: process.env.MAPS_API_KEY,
+    AWSAccessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    AWSSecretKey: process.env.AWS_SECRET_KEY,
+    AWSBucket: process.env.AWS_BUCKET,
+    DarkSkyAPIKey: process.env.DARK_SKY_API_KEY
+}
